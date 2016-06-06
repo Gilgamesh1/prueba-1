5dfe19d917157f49d6f5a88502a431778762c8d6
Gilgamesh1 <rgllv@hotmail.com>
2016-06-05 22:17:07 -0500

    MtnCliente js backup

--- SERMED/web/recursos/js/cu_mtncliente/mtncliente.js
+++ SERMED/web/recursos/js/cu_mtncliente/mtncliente.js
@@ -8,101 +8,74 @@ $(document).ready(function () {
     $("#msj-sucess").fadeIn();
 });
 
-function buscarClientes() {
-    var b1 = $("#t1").val();
-    var b2 = $("#t2").val();
-    var url = "MtnCliente/buscar.html?b1=" + b1 + "&b2=" + b2;
-    var form = $("#form");
-    form.attr("action", url);
-    form.submit();
-    form.attr("action", "MtnCliente/buscar.html");
-}
-//onclick="buscarClientes()"
-function guardarCliente() {
-    var val = $("#t9").val();
-    var form = $("#form");
-    if (val == 0)
-        form.attr("action", "insertar.html");
-    else
-        form.attr("action", "actualizar.html");
-    form.submit();
-    form.attr("action", "insertarMtnCliente.html");
+function anterior(nro) {
+
 }
 
-function agregarDireccion() {
-    var form = $("#form");
-    var url = path;
-    if ($("#t11").val() == "x") {
-        url +="/MtnCliente/agregarDireccion.html?b1=" + $("#t8").val() + "&b2=" + $("#t10").val();
-    } else {
-        url +="/MtnCliente/actualizarDireccion.html?b1=" + $("#t8").val() + "&b2=" + $("#t10").val() + "&b3=" + $("#t11").val();
-    }
-    form.attr("action", url);
-//    form.submit();
-//    form.attr("action", "insertarMtnCliente.html");
-}
-
-function cargarDireccion(pos) {
-    var form = $("#form");
-    var url = path + "/MtnCliente/cargarDireccion.html?b3=" + pos;
-    form.attr("action", url);
-    form.submit();
-    form.attr("action", "insertarMtnCliente.html");
-}
-
-//function actualizarDireccion(pos) {
-//    var form = $("#form");
-//    var url = path + "/MtnCliente/actualizarDireccion.html?b1=" + $("#t8").val() + "&b2=" + $("#t9").val() + "&b3=" + pos;
-//    form.attr("action", url);
-//    form.submit();
-//    form.attr("action", "insertarMtnCliente.html");
-//}
-
-function eliminar(valor){
-    var form = $("#form");
-    var url = path +"/MtnCliente/eliminarDireccion.html?id=" + valor;
-    form.attr("action", url);
-    form.submit();
-}
-
-
-function json(){
-//    var form = $("#form");//MtnCliente/
-//    var url = path +"/buscarMtnCliente/json.html?b1=quiero&b2=sexo";
-//    form.attr("action", url);
-//    form.submit();
-    var data = {id:1,nom:"loquillo"};
-    console.log(data);
-    console.log(JSON.stringify(data));
-    
-    var x = {}
-    x["query"] = "wqeqwe";
-    console.log(x);
-    console.log(JSON.stringify(x));
-    
+function pagina(nro) {
+    var datos = {nombre: $("#t1").val(), documento: $("#t2").val(), nro: nro};
+    console.log(datos);
+    console.log(JSON.stringify(datos));
+    var url = $("#context").val() + "/MtnCliente/pagina.html";
+    console.log(url);
     $.ajax({
-        type:"POST",
-        contentType : "application/json",
-        data : JSON.stringify(data),
-        url : "http://localhost:8084/SERMED/json1.html",
-        dataType : 'json',
-        timeout : 100000,
-        success : function(data) {
-                console.log("SUCCESS: ", data);
-                display(data);
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(datos),
+        url: url,
+        dataType: 'json',
+        timeout: 100000,
+        success: function (data) {
+            console.log("SUCCESS: ", data);
+            console.log("longitud: ", data.length);
+            console.log(JSON.stringify(data));
+            var filas="";
+            for (i = 0; i < data.length; i++) {
+                var val = data[i];
+                console.log(JSON.stringify(val));
+                console.log(val['id'] + ", " + val['nombre'] + ", " + val['valDocumento'] + ", " + val['nroTabla']);
+                filas+="<tr><td>"+val['nroTabla']+"</td><td>"+val['nombre']+
+                        "</td><td>"+val['dir']+"</td><td>"+val['ubi']+"</td><td>"+val['valDocumento']+
+                "</td><td>"+val['representante']+"</td><td>"+val['telefono']+"</td><td>"+val['celular']+"</td><td>"+
+                "<a href='cargarUnCliente.html?id='"+val['id']+" class='btn btn-default' role='button'>Actualizar</a></td></tr>";
+            }
+            
+//            document.getElementById("demo").innerHTML = cabecera();
+            $("#contenido").remove();
+            var tbody=$('<tbody/>',{'id':'contenido'});
+            var tabla = '<table class="table table-bordered" border="0">';
+            tabla += '<tr><td>Celda 1</td><td>Celda 2</td><td> Celda 3</td></tr>';
+            tabla += '</table>';
+//            $("#tabla").append("<tbody><tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td></tr></tbody>");
+//            var tbody=document.createElement("tbody");
+            $("#tabla").append(tbody);
+            $("#contenido").append(filas);
+
+            var txt = "";
+            var person = {fname: "John", lname: "Doe", age: 25};
+            var x;
+            for (x in person) {
+                txt += person[x] + " ";
+            }
+            document.getElementById("demo").innerHTML = tabla;
+
         },
-        error : function(e) {
-                console.log("ERROR: ", e);
-                display(e);
+        error: function (e) {
+            console.log("ERROR: ", e);
         },
-        done : function(e) {
-                console.log("DONE");
+        done: function (e) {
+            console.log("DONE");
         }
     });
 }
 
-	function display(data) {
-		var json = "<h4>Ajax Response</h4><pre>"
-				+ JSON.stringify(data, null, 4) + "</pre>";
-		$('#feedback').html(json);
-	}
+function siguiente(nro) {
+
+}
+
+function cabecera() {
+    var cabecera = "<table class=\"\ table table-bordered \"\><thead><tr><th>#</th><th>\n\
+Nombre</th><th>Direcci&oacute;n</th><th>Ciudad</th><th>DNI - RUC</th><th>Repre. Legal</th><th>\n\
+Tel&eacute;fono</th><th>Celular</th><th>Actualizar</th></tr></thead></table>";
+    console.log(cabecera);
+}
